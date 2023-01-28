3e238f169ac4a67d4093027e1a6236df94cc174a
Atif Aqeel <atifaqeel430@gmail.com>
2023-01-29 00:20:21 +0500

    Reverse a Linked List

---
+++ Reverse a linked List.cpp
@@ -0,0 +1,96 @@
+/*
+Reverse a linked list by Iterative Method:
+Follow the steps below to solve the problem:
+
+-Initialize three pointers prev as NULL, curr as head, and next as NULL.
+-Iterate through the linked list. In a loop, do the following:
+	-Before changing the next of curr, store the next node 
+		-next = curr -> next
+	-Now update the next pointer of curr to the prev 
+		-curr -> next = prev 
+	-Update prev as curr and curr as next 
+		-prev = curr 
+		-curr = next
+
+just reverse the arrpw heads of ll
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node* next;
+	
+	Node(int data)
+	{
+		this->data = data;
+		next = NULL;
+	}
+};
+
+struct LinkedList {
+	Node* head;
+	LinkedList() { 
+		head = NULL; 
+	}
+
+	/* Function to reverse the linked list */
+	void reverse()
+	{
+		// Initialize current, previous and next pointers
+		Node* current = head;
+		Node *prev = NULL;
+		Node *next = NULL;
+
+		while (current != NULL) {
+			// Store next
+			next = current->next;
+			// Reverse current node's pointer
+			current->next = prev;
+			// Move pointers one position ahead.
+			prev = current;
+			current = next;
+		}
+		head = prev;
+	}
+
+	/* Function to print linked list */
+	void print()
+	{
+		struct Node* temp = head;
+		while (temp != NULL) {
+			cout << temp->data << " ";
+			temp = temp->next;
+		}
+	}
+
+	void push(int data)
+	{
+		Node* temp = new Node(data);
+		temp->next = head;
+		head = temp;
+	}
+};
+
+int main()
+{
+	/* Start with the empty list */
+	LinkedList ll;
+	ll.push(20);
+	ll.push(4);
+	ll.push(15);
+	ll.push(85);
+
+	cout << "Given linked list\n";
+	ll.print();
+
+	ll.reverse();
+
+	cout << "\nReversed linked list \n";
+	ll.print();
+
+return 0;
+}
+
